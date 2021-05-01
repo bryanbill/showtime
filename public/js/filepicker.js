5163275b411c9c1f2255707f2774db22469155e8
cocoginger <emobrya@gmail.com>
2021-05-01 15:51:36 +0300

    It's ShowTime

---
+++ public/js/filepicker.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#input-b6").fileinput({
+      showUpload: false,
+      dropZoneEnabled: false,
+      maxFileCount: 10,
+      mainClass: "input-group-lg"
+  });
+});
